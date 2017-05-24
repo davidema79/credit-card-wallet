78339285b1502c4fcc965862f08225e0bb934399
DESKTOP-BDHJU7J\davide <davide.martorana@gmail.com>
2017-05-24 21:40:47 +0200

    Imported classes

--- wallet-backend/wallet-parent/wallet-gateway/src/main/java/eu/davidem/wallet/gateway/ApplicationLauncher.java
+++ wallet-backend/wallet-parent/wallet-gateway/src/main/java/eu/davidem/wallet/gateway/ApplicationLauncher.java
@@ -1,32 +1,36 @@
 package eu.davidem.wallet.gateway;
 
-import java.util.logging.Logger;
 
-// @SpringBootApplication
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApplicationLauncher {
-//	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationLauncher.class);
-//
-//	public static void main(String[] args) {
-//
-//		LOGGER.info("############################################################################################");
-//		LOGGER.info("###########            APPLICATION eWALLET FOR CREDIT CARD STARTING....          ###########");
-//		LOGGER.info("############################################################################################");
-//		try {
-//			SpringApplication.run(ApplicationLauncher.class, args);
-//		} catch (final Exception e) {
-//			LOGGER.info("############################################################################################################");
-//			LOGGER.info("###########        ....APPLICATION eWALLET FOR CREDIT CARD FAILED TO START       ###########################");
-//			LOGGER.info("############################################################################################################");
-//			LOGGER.error("Exception: {}", e.getMessage(), e);
-//			
-//			System.exit(-1);
-//		}
-//
-//		LOGGER.info("############################################################################################");
-//		LOGGER.info("###########            .....APPLICATION eWALLET FOR CREDIT CARD STARTED          ###########");
-//		LOGGER.info("############################################################################################");
-//	}
-//	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationLauncher.class);
+
+	public static void main(String[] args) {
+
+		LOGGER.info("############################################################################################");
+		LOGGER.info("###########            APPLICATION eWALLET FOR CREDIT CARD STARTING....          ###########");
+		LOGGER.info("############################################################################################");
+		try {
+			SpringApplication.run(ApplicationLauncher.class, args);
+		} catch (final Exception e) {
+			LOGGER.info("############################################################################################################");
+			LOGGER.info("###########        ....APPLICATION eWALLET FOR CREDIT CARD FAILED TO START       ###########################");
+			LOGGER.info("############################################################################################################");
+			LOGGER.error("Exception: {}", e.getMessage(), e);
+			
+			System.exit(-1);
+		}
+
+		LOGGER.info("############################################################################################");
+		LOGGER.info("###########            .....APPLICATION eWALLET FOR CREDIT CARD STARTED          ###########");
+		LOGGER.info("############################################################################################");
+	}
+	
 	
 	
 }
