13a74df30848457dc44c0d9e78ce11bf41060672
DESKTOP-BDHJU7J\davide <davide.martorana@gmail.com>
2017-05-24 21:39:19 +0200

    Created structure for Backend

---
+++ wallet-backend/wallet-parent/wallet-gateway/src/main/java/eu/davidem/wallet/gateway/ApplicationLauncher.java
@@ -0,0 +1,32 @@
+package eu.davidem.wallet.gateway;
+
+import java.util.logging.Logger;
+
+// @SpringBootApplication
+public class ApplicationLauncher {
+//	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationLauncher.class);
+//
+//	public static void main(String[] args) {
+//
+//		LOGGER.info("############################################################################################");
+//		LOGGER.info("###########            APPLICATION eWALLET FOR CREDIT CARD STARTING....          ###########");
+//		LOGGER.info("############################################################################################");
+//		try {
+//			SpringApplication.run(ApplicationLauncher.class, args);
+//		} catch (final Exception e) {
+//			LOGGER.info("############################################################################################################");
+//			LOGGER.info("###########        ....APPLICATION eWALLET FOR CREDIT CARD FAILED TO START       ###########################");
+//			LOGGER.info("############################################################################################################");
+//			LOGGER.error("Exception: {}", e.getMessage(), e);
+//			
+//			System.exit(-1);
+//		}
+//
+//		LOGGER.info("############################################################################################");
+//		LOGGER.info("###########            .....APPLICATION eWALLET FOR CREDIT CARD STARTED          ###########");
+//		LOGGER.info("############################################################################################");
+//	}
+//	
+	
+	
+}
